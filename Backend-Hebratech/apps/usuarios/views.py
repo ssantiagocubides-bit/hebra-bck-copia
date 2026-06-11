3e1fd31020b612f076cc830b5806453403742f1b
juan alba <juanandresalbacastro2007@gmail.com>
2026-06-11 10:10:30 -0500

    repossubmit

---
+++ Backend-Hebratech/apps/usuarios/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Vista para el Login (ya la tenías, asegúrate de que apunte al nombre exacto de tu archivo)
+def login_view(request):
+    return render(request, 'usuarios/login.html')
+
+# Nueva Vista para Recuperar Contraseña
+def recuperar_view(request):
+    return render(request, 'usuarios/recuperar.html')
